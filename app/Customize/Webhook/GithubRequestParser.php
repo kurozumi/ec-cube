8e225a8ede7567dd6a8bc52d54a6a9fef0e4b97e
kurozumi <info@a-zumi.net>
2024-09-10 19:42:44 +0900

    fix

--- app/Customize/Webhook/GithubRequestParser.php
+++ app/Customize/Webhook/GithubRequestParser.php
@@ -81,6 +81,7 @@ final class GithubRequestParser extends AbstractRequestParser
 
     protected function validatePayload(InputBag $payload): void
     {
+        // ??
         if ($payload->has('action') && 'closed' !== $payload->get('action')) {
             throw new RejectWebhookException(406, 'Pull Request is not closed.');
         }
